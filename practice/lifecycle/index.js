88e4e41b3f212dc2605c5dfe1e5a625f8d279494
guangtt <bbb3520@163.com>
2018-12-04 11:04:25 +0800

    add lifecycle

--- practice/lifecycle/index.js
+++ practice/lifecycle/index.js
@@ -1,12 +1,46 @@
-import Vue from '.vue'
+import Vue from 'vue'
 
-new Vue({
+var app = new Vue({
     el: '#root',
-    template: '<div>{{text}}</div>',
+    template: '<div ref="text">{{text}}</div>', //解析为render方法
     data: {
-        text: 'aaa'
+        text: 0
     },
     beforeCreate () {
-
+        console.log(this.$el, 'beforeCreate')
+    },
+    // 对数据的操作在created和mounted都行
+    created () {
+        console.log(this.$el, 'created')
+    },
+    beforeMount () {
+        console.log(this.$el, 'beforeMount')
+    },
+    // render在beforeMount和mounted之间执行，优先级最高
+    // render (h) {
+    //     return h('div', {}, this.text)
+    // },
+    // 对DOM节点进行的操作一般在mounted
+    mounted () {
+        console.log(this.$el, 'mouted')
+    },
+    beforeUpdate () {
+        console.log('beforeUpdate')
+    },
+    updated () {
+        console.log('updated')
+    },
+    beforeDestroy () {
+        console.log('beforeDestroy')
+    },
+    destroyed () {
+        console.log('destroy')
     },
 })
+
+// setInterval(()=>{
+//     app.text += 1;
+// }, 2000)
+// setTimeout(() => {
+//     app.$destroy(); // 销毁组件
+// }, 1000)
